4daaa75ae5fc52371fc8fb6036729dd7355fd2a7
Ceslovas Karzenauskas <jesse@liveradio.ie>
2025-06-18 12:06:17 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('✅ Hello from TechCo CI/CD Server! 2025');
+  res.end('✅ Hello from TechCo CI/CD Server! 2025!');
 });
 
 server.listen(port, () => {
